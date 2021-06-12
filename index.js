d4ef9787608fcb2754ab915101081a45623f21b1
Kayla Jeter <JeterK@spu.edu>
2021-06-11 22:28:34 -0700

    updated port on index

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.delete('/del_user', function HandleUserDel(request, response) {
  })
  
  const port = process.env.PORT || 3000;
- var server = app.listen(3000, function () {
+ var server = app.listen(port, function () {
  
     var host = server.address().address
     var port = server.address().port
